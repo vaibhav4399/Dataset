c410b695bf316e5060d990d65cc0fa511a256075
Vaibhav4399 <vaibhavprabhu19@gmail.com>
2021-03-23 06:52:33 +0530

    Database

--- api/api.py
+++ api/api.py
@@ -8,21 +8,6 @@ app.url_map.strict_slashes = False
 cors = CORS(app)
 
 
-@app.route("/")
-def home():
-    return make_response(jsonify(message="Hello"), 200)
-
-
-@app.route("/about/")
-def about():
-    return make_response(jsonify(message="about"), 200)
-
-
-@app.route("/contact/")
-def contact():
-    return make_response(jsonify(message="contacts"), 200)
-
-
 app.register_blueprint(user, url_prefix="/user")
 
 if __name__ == "__main__":
--- api/user/models.py
+++ api/user/models.py
@@ -1,18 +1,36 @@
 from flask import jsonify, request, make_response
 from config.connect import db
+from bson import json_util
+import json
 
 
 class User:
 
     def register(self):
 
-        user = {
-            "name": request.json.get("name"),
-            "email": request.json.get("email"),
-            "password": request.json.get("password")
-        }
+        if request.json.get("name") and request.json.get("email") and request.json.get("password"):
+            user = {
+                "name": request.json.get("name"),
+                "email": request.json.get("email"),
+                "password": request.json.get("password")
+            }
 
-        return make_response(jsonify(user), 200)
+            if db.users.find_one({"email": user["email"]}):
+                error = {
+                    "error": True,
+                    "message": "User Already Exists!"
+                }
+                return make_response(jsonify(error), 403)
 
+            else:
+                db.users.insert_one(user)
+                user["error"] = False
+                new = json.loads(json_util.dumps(user))
+                return make_response(jsonify(new), 200)
 
-db.users.insert_one({"name": "vaibhav"})
+        else:
+            error = {
+                "error": True,
+                "message": "User Already Exists!"
+            }
+            return make_response(jsonify(error), 400)
